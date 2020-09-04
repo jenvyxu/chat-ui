c9f1828f66b296235a24df555df743136f3af6dd
xzw <1272117264@qq.com>
2020-09-04 08:24:08 +0800

    开发Icon组件

--- src/components/Icon/icon.stories.js
+++ src/components/Icon/icon.stories.js
@@ -1,9 +1,52 @@
 import React from "react";
 import Icon from ".";
+import { ReactComponent as SmileIcon } from "assets/icons/smile.svg";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCommentDots,
+  faFolder,
+  faStickyNote,
+} from "@fortawesome/free-solid-svg-icons";
 
 export default {
-  title: "Icon",
-  components: Icon
-}
+  title: "UI 组件/Icon",
+  components: Icon,
+};
 
-export const Default = () => <Icon>默认</Icon>;
+export const Default = () => <Icon icon={SmileIcon} />;
+
+export const CustomColor = () => {
+  return <Icon icon={SmileIcon} color={"#cccccc"} />;
+};
+
+export const CustomSize = () => {
+  return <Icon icon={SmileIcon} width={48} height={48} color={"#cccccc"} />;
+};
+
+export const FontAwesome = () => {
+  return <FontAwesomeIcon icon={faCommentDots} style={{ fontSize: "20px" }} />;
+};
+
+export const FontAwesomeColor = () => {
+  return <FontAwesomeIcon icon={faCommentDots} style={{ color: "#cccccc" }} />;
+};
+
+export const FontAwesomeSizes = () => {
+  return (
+    <div>
+      <FontAwesomeIcon
+        icon={faFolder}
+        style={{ fontSize: "24px", color: "#cccccc" }}
+      />
+      <FontAwesomeIcon
+        icon={faStickyNote}
+        style={{ fontSize: "36px", color: "#cccccc" }}
+      />
+      <FontAwesomeIcon
+        icon={faCommentDots}
+        style={{ fontSize: "48px", color: "#cccccc" }}
+      />
+    </div>
+  );
+};
