0c6491478bda637b0f1fb0614836890c66d37858
xzw <1272117264@qq.com>
2020-09-04 11:12:59 +0800

    开发Input组件

---
+++ src/components/Input/input.stories.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Input from ".";
+import Icon from "components/Icon";
+import { ReactComponent as ClipIcon } from "assets/icons/clip.svg";
+import { ReactComponent as SmileIcon } from "assets/icons/smile.svg";
+
+export default {
+  title: "UI 组件/Input",
+  components: Input,
+};
+
+export const Default = () => <Input />;
+
+export const Search = () => <Input.Search />;
+
+export const WithAffix = () => (
+  <Input
+    prefix={<Icon icon={ClipIcon} color="#cccccc" />}
+    suffix={<Icon icon={SmileIcon} color="#cccccc" />}
+  />
+);
