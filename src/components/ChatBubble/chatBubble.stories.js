6ee4c3f089211c663693bab3509a7be238905586
xzw <1272117264@qq.com>
2020-09-05 20:00:23 +0800

    开发语音消息组件

--- src/components/ChatBubble/chatBubble.stories.js
+++ src/components/ChatBubble/chatBubble.stories.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ChatBubble from ".";
+import VoiceMessage from "components/VoiceMessage";
 
 export default {
   title: "UI 组件/ChatBubble",
@@ -11,8 +12,20 @@ export const FromOthers = () => (
   <ChatBubble time="昨天 下午14:26">这是一条来自他人发送的聊天消息</ChatBubble>
 );
 
-export const FromMe = () => (
+export const Mine = () => (
   <ChatBubble time="昨天 下午14:26" type="mine">
     这是一条我自己发送的聊天消息
+  </ChatBubble>
+);
+
+export const VoiceMessageType = () => (
+  <ChatBubble time="昨天 下午16: 30">
+    <VoiceMessage time="01:24" />
+  </ChatBubble>
+);
+
+export const VoiceMessageTypeMine = () => (
+  <ChatBubble type="mine" time="昨天 下午16: 30">
+    <VoiceMessage type="mine" time="01:24" />
   </ChatBubble>
 );
